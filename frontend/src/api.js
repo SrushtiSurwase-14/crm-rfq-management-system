dbcddd34321a3a7ad2deea2481e0c23f452508b5
srushti surwase <srushtisurwase60@gmail.com>
2026-09-08 21:34:51 +0530

    Connect frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://crm-rfq-management-system-ikl6.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
